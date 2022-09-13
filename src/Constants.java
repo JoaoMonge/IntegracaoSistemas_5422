3b669a2c96740103ed16d44bb5bbd913256bc0a3
JoaoMonge <joao.monge13@gmail.com>
2022-09-13 17:55:52 +0100

    Adicionado o refresh rate que permite modificar mais facilmente o tempo em que se verifica se existe uma nova reserva.

--- src/Constants.java
+++ src/Constants.java
@@ -3,6 +3,8 @@ public class Constants {
     public static String DATABASE_URL = "jdbc:mysql://localhost:3306/Hotel_5086?characterEncoding=utf8";
     public static String DATABASE_USER = "root";
     public static String DATABASE_PASSWORD = "p@ssw0rd";
+
+    //Caminho para o ficheiro jasperreports.
     public static String FILEPATH_JASPERREPORT=  "/Users/joaomonge/IdeaProjects/IntegracaoSistemas/ConfirmacaoReserva.jrxml";
 
     //Equivale a verificar de 2 em 2 minutos se existe uma nova reserva.
