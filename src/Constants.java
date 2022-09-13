abc9897fafcc7600b27ed8b77441427c1e9cdc1c
JoaoMonge <joao.monge13@gmail.com>
2022-09-13 18:35:35 +0100

    Alteração do caminho para o ficheiro jaspersoft

--- src/Constants.java
+++ src/Constants.java
@@ -4,10 +4,10 @@ public class Constants {
     public static String DATABASE_USER = "root";
     public static String DATABASE_PASSWORD = "p@ssw0rd";
 
-    //Caminho para o ficheiro jasperreports. ALTERAR PARA O VOSSO
+    //Caminho para o ficheiro jasperreports.
     public static String FILEPATH_JASPERREPORT=  "ConfirmacaoReserva.jrxml";
 
     //Equivale a verificar de 2 em 2 minutos se existe uma nova reserva.
-    public static int REFRESH_RATE = 120;
+    public static int REFRESH_RATE = 720;
 
 }
